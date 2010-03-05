6ff31421d8186a4c0d7fa521a7d9df770b3d6cf6
Fabien DUMINY <fabien.duminy@webmails.com>
2010-03-05 22:53:02 +0100

    added tests for domain's ip address

--- client/src/test/java/org/jtestplatform/client/domain/libvirt/TestLibVirt.java
+++ client/src/test/java/org/jtestplatform/client/domain/libvirt/TestLibVirt.java
@@ -22,13 +22,21 @@
  */
 package org.jtestplatform.client.domain.libvirt;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+
 import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
+import org.apache.log4j.Logger;
 import org.jtestplatform.client.ConfigReader;
 import org.jtestplatform.client.domain.ConfigurationException;
 import org.jtestplatform.client.domain.Domain;
 import org.jtestplatform.client.domain.DomainConfig;
+import org.jtestplatform.common.ConfigUtils;
 import org.jtestplatform.configuration.Configuration;
 import org.jtestplatform.configuration.Connection;
 import org.junit.After;
@@ -41,6 +49,10 @@ import org.junit.Test;
  *
  */
 public class TestLibVirt {
+    private static final Logger LOGGER = Logger.getLogger(TestLibVirt.class);
+    
+    private static final int NB_PINGS = 5;
+    
     private Connection connection;
     
     private LibVirtDomainFactory factory;
@@ -65,14 +77,37 @@ public class TestLibVirt {
     @Test
     public void testStart() throws ConfigurationException, IOException {
         domain = factory.createDomain(createDomainConfig(), connection);
-        domain.start();
+        start(domain);
     }
     
     @Test
     public void testStop() throws ConfigurationException, IOException {
         domain = factory.createDomain(createDomainConfig(), connection);
-        domain.start();
+        String ip = start(domain);
+        
         domain.stop();
+        assertNull(domain.getIPAddress());
+        assertEquals("after stop, domain must not be pingable", 0, ping(ip));
+    }
+    
+    private String start(Domain domain) throws IOException, ConfigurationException {
+        String ip = domain.getIPAddress();
+        assertNull(ip);
+        
+        ip = domain.start();
+        
+        // wait a bit that the system has started
+        try {
+            Thread.sleep(40000);
+        } catch (InterruptedException e) {
+            // ignore
+        }
+        
+        assertFalse("ip must not be null, empty or blank", ConfigUtils.isBlank(ip));
+        assertEquals("domain must be pingable", NB_PINGS, ping(ip));
+        assertEquals("domain.start() must return same ip address as domain.getIpAddress()", ip, domain.getIPAddress());
+        
+        return ip;
     }
 
     /**
@@ -84,4 +119,21 @@ public class TestLibVirt {
         cfg.setCdrom(new File(config.getWorkDir()).getParent() + File.separatorChar + "config" + File.separatorChar + "microcore_2.7.iso");
         return cfg;
     }
+    
+    private int ping(String host) throws UnknownHostException, IOException {
+        LOGGER.debug("pinging " + host);
+        
+        final int timeOut = 1000;
+        final InetAddress addr = InetAddress.getByName(host);
+        
+        int counter = 0;
+        for (int i = 0; i < NB_PINGS; i++) {
+            if (addr.isReachable(timeOut)) {
+                counter++;
+                LOGGER.debug("received pong from " + host);
+            }
+        }
+        
+        return counter;
+    }    
 }
